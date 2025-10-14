0f68f458f161b52b7952b3bfe9092cf194f0607b
Qu7001560 <7001560@spsd.sk.ca>
2025-10-14 09:12:14 -0600

    perlin noise demo

---
+++ perlin-noise/sketch.js
@@ -0,0 +1,24 @@
+// perlin noise demo
+
+let x;
+let y;
+let time = 0;
+const TIME_BUFFER = 1000;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  // move ball
+  x = noise(time)*width;
+  y = noise(time+TIME_BUFFER)*height;
+  time+=0.01;
+
+  // display ball
+  fill("black");
+  circle(x, y, 50);
+}
