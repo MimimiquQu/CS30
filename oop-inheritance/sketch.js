547944de9612774af732a622e4b11af49578890b
MimimiquQu <tschumiq@gmail.com>
2025-11-19 09:08:56 -0600

    oop inheritance demo

--- oop-inheritance/sketch.js
+++ oop-inheritance/sketch.js
@@ -1,13 +1,34 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// OOP Inheritance
 
+class Unit {
+  constructor(index, type) {
+    this.index = index;
+    this.type = type;
+  }
+
+  train() {
+    console.log("Training unit #" + (this.index+1) + " of type " + this.type);
+  }
+}
+
+class Footman extends Unit {
+  constructor(index) {
+    super(index, 'footman')
+  }
+
+  train() {
+    console.log("A footman costs 135 gold and 2 food.");
+    super.train();
+  }
+}
+
+let u1 = new Unit(0, 'footman');
+let f1 = new Footman(1);
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  u1.train();
+  f1.train();
 }
 
 function draw() {
